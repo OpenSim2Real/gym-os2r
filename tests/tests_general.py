2c43d064d04411cf585c4685e44eb97ba318b0dd
HorvathDawson <horvath.dawson@gmail.com>
2021-10-04 13:44:19 -0700

    Updated the tests to use new reward class.

--- tests/tests_general.py
+++ tests/tests_general.py
@@ -23,7 +23,7 @@ def check_registered_envs():
     for env_id in env_ids:
         make_env = functools.partial(make_env_from_id, env_id=env_id)
         env = randomizers.monopod.MonopodEnvRandomizer(
-            env=make_env, reward_type='Balancing_v1')
+            env=make_env, reward_class_name='BalancingV1')
         env.seed(42)
         # Test if env adheres to Gym API
         check_env(env, warn=True, skip_render_check=True)
@@ -71,7 +71,7 @@ def test_random_rollout():
     for env_id in env_ids:
         make_env = functools.partial(make_env_from_id, env_id=env_id)
         env = randomizers.monopod.MonopodEnvRandomizer(
-            env=make_env, reward_type='Balancing_v1')
+            env=make_env, reward_class_name='BalancingV1')
         env.seed(42)
         def agent(ob): return env.action_space.sample()
         ob = env.reset()
@@ -89,7 +89,7 @@ def single_process():
     env_id = "Monopod-v1"
     make_env = functools.partial(make_env_from_id, env_id=env_id)
     env = randomizers.monopod_no_rand.MonopodEnvNoRandomizer(
-        env=make_env, reward_type='Balancing_v1')
+        env=make_env, reward_class_name='BalancingV1')
     env.seed(42)
     observation = env.reset()
 
@@ -112,7 +112,7 @@ def single_process_fixed_hip():
     env_id = "Monopod-fh-v1"
     make_env = functools.partial(make_env_from_id, env_id=env_id)
     env = randomizers.monopod_no_rand.MonopodEnvNoRandomizer(
-        env=make_env, reward_type='Balancing_v1')
+        env=make_env, reward_class_name='BalancingV1')
     env.seed(42)
     observation = env.reset()
 
@@ -134,7 +134,7 @@ def test_monopod_model():
     env_id = "Monopod-v1"
     make_env = functools.partial(make_env_from_id, env_id=env_id)
     env = randomizers.monopod.MonopodEnvRandomizer(
-        env=make_env, reward_type='Balancing_v1')
+        env=make_env, reward_class_name='BalancingV1')
     env.seed(42)
     observation = env.reset()
     assert len(observation) == 10, "base monopod should have 10 observations"
@@ -143,7 +143,7 @@ def test_monopod_model():
     env_id = "Monopod-fh-v1"
     make_env = functools.partial(make_env_from_id, env_id=env_id)
     env = randomizers.monopod.MonopodEnvRandomizer(
-        env=make_env, reward_type='Balancing_v1')
+        env=make_env, reward_class_name='BalancingV1')
     observation = env.reset()
     assert len(observation) == 8, "fixed hip monopod should have 8 observations"
     env.close()
@@ -151,7 +151,7 @@ def test_monopod_model():
     env_id = "Monopod-fh-fby-v1"
     make_env = functools.partial(make_env_from_id, env_id=env_id)
     env = randomizers.monopod.MonopodEnvRandomizer(
-        env=make_env, reward_type='Balancing_v1')
+        env=make_env, reward_class_name='BalancingV1')
     observation = env.reset()
     assert len(
         observation) == 6, "fixed hip and fixed boom yaw monopod should have 6 observations"
@@ -160,7 +160,7 @@ def test_monopod_model():
 # def multi_process_fixed_hip():
     # env_id = "Monopod-v1"
     # make_env = functools.partial(make_env_from_id, env_id=env_id)
-    # env = randomizers.monopod_no_rand.MonopodEnvNoRandomizer(env=make_env, reward_type='Balancing_v1')
+    # env = randomizers.monopod_no_rand.MonopodEnvNoRandomizer(env=make_env, reward_class_name='BalancingV1')
     # env.seed(42)
     # observation = env.reset()
     #
