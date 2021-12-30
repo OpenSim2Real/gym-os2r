a9a24ffca1355ab4d5c342f5f756d7dc9376d91b
Dawson <horvath.dawson@gmail.com>
2021-12-30 12:25:45 -0800

    test new doc strings

--- gym_os2r/models/__init__.py
+++ gym_os2r/models/__init__.py
@@ -2,6 +2,6 @@ from . import monopod
 from . import config
 from . import models
 
-from .config.config import SettingsConfig
+from .config import SettingsConfig
 
 __all__ = ['monopod', 'config', 'models', 'SettingsConfig']
