2f4d22acecd4b9c602dd1fb64479a36f046bda0f
Dawson Horvath <horvath.dawson@gmail.com>
2021-12-31 19:26:59 -0800

    upate reward utils docs

--- gym_os2r/rewards/rewards_utils.py
+++ gym_os2r/rewards/rewards_utils.py
@@ -84,8 +84,7 @@ def tolerance(x, bounds=(0.0, 0.0), margin=0.0, sigmoid='gaussian',
             bounds for the target interval. These can be infinite if the interval is
             unbounded at one or both ends, or they can be equal to one another if
             the target value is exact.
-        margin (float) : Parameter that controls how steeply the output decreases as `x`
-            moves out-of-bounds.
+        margin (float) : Parameter that controls how steeply the output decreases as `x` moves out-of-bounds.
                 * If `margin == 0` then the output will be 0 for all values of `x` outside of `bounds`.
                 * If `margin > 0` then the output will decrease sigmoidally with increasing distance from the nearest bound.
         sigmoid (String) : choice of sigmoid type Valid values are:
