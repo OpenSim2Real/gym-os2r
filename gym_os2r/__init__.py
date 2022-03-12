d8f999b91f70fd6214352e9d07135f1b699c3d53
Dawson Horvath <horvath.dawson@gmail.com>
2022-03-12 01:01:20 -0800

    fixed reward scaling

--- gym_os2r/__init__.py
+++ gym_os2r/__init__.py
@@ -20,7 +20,7 @@ register(
             'agent_rate': 1000,
             'physics_rate': 10000,
             'real_time_factor': max_float,
-            'task_mode': 'free_hip',
+            'task_mode': 'fixed_hip',
             'reward_class': StandingV3,
             'reset_positions': ['ground']
             })
